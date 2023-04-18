96eeff11cea188c3695946be3b042201635918b7
Ercan <eposta@ercanermis.com>
2023-04-18 17:56:25 +0300

    add: init

---
+++ README.md
@@ -0,0 +1,115 @@
+
+``# S3-Directory-Listing 📂🎉
+
+Welcome to the **S3-Directory-Listing** project! Are you tired of the boring, generic S3 bucket listings? 🥱 Well, look no further! 😎 This snazzy little JS script and HTML combo will turn your S3 bucket into a fancy file and folder listing with a nice UI and search functionality. Oh, and did I mention it also has a dark mode? 🌙
+
+## Usage 🚀
+
+To use S3-Directory-Listing, just follow these easy-peasy steps:
+
+1. Clone this repo or copy the contents of ,`dark-mode.css`, `s3.js` and `index.html` to your S3 bucket.
+2. Update the `bucketName` variable in `s3.js` with your bucket's name.
+3. Configure your S3 bucket settings (see section below).
+4. Access the `index.html` file in your browser, and voilà! 🎩✨ You now have a fancy S3 directory listing.
+
+## S3 Bucket Settings 🔧
+
+To make this magic work, you need to configure your S3 bucket settings properly:
+
+1. Make sure your S3 bucket is publicly accessible (or at least accessible to the users you want to share the directory listing with).
+2. Set the bucket policy to allow public read access to your objects. Use the following policy, replacing `<your-bucket-name>` with your actual bucket name:
+
+```json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "S3DirectoryListing",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject",
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::<your-bucket-name>/*",
+                "arn:aws:s3:::<your-bucket-name>"
+            ]
+        }
+    ]
+}
+``` 
+Or you can use an IP Policy for your bucket if you need.
+
+```json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "S3DirectoryListing",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject",
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::<your-bucket-name>/*",
+                "arn:aws:s3:::<your-bucket-name>"
+            ],
+            "Condition": {
+                "IpAddress": {
+                    "aws:SourceIp": "<your-ip-address>"
+                }
+            }
+        }
+    ]
+}
+```
+
+Replace `<your-ip-address>` with your desired IP address or a CIDR block to restrict access to specific IP addresses.
+
+3.  Enable static website hosting for your bucket:
+    -   Go to your S3 bucket settings on the AWS Management Console.
+    -   Select the "Properties" tab.
+    -   Scroll down to "Static website hosting" and click "Edit".
+    -   Choose "Enable" and set the "Index document" and "Error document" to `index.html`.
+    -   Save your changes.
+
+4. Update your Cross-origin resource sharing (CORS) settings like below.
+```
+[
+    {
+        "AllowedHeaders": [
+            "*"
+        ],
+        "AllowedMethods": [
+            "GET"
+        ],
+        "AllowedOrigins": [
+            "*"
+        ],
+        "ExposeHeaders": []
+    }
+]
+```
+
+5.  Note down the bucket's static website endpoint (e.g., `http://<your-bucket-name>.s3-website-<your-region>.amazonaws.com/index.html` or `https://<your-bucket-name>.s3.amazonaws.com/index.html` for secure connection). This is where you can access your fancy directory listing.
+
+That's it! Now you can enjoy your brand new, fancy-pants S3 directory listing! 🕺💃
+
+## A Little Extra ✨
+
+You can customize the number of items shown per page by modifying the `itemsPerPage` variable in `s3.js`.
+
+`
+const itemsPerPage = 10; // Change this number to your desired items per page
+`
+
+## Final Words 📝
+
+Feel free to share this project with your friends, colleagues, or even your grandma! 🧓 After all, who doesn't like a fancy directory listing? 😏
+
+If you have any questions, comments, or jokes to share, head over to the [GitHub repo](https://github.com/flightlesstux/S3-Directory-Listing) and let us know. We'd love to hear from you! 🤗
+
+Happy listing! 📚
---
+++ s3.js
@@ -0,0 +1,208 @@
+// S3 bucket name
+const bucketName = 's3-directory-listing';
+
+const objectList = document.getElementById('object-list');
+const breadcrumb = document.getElementById('breadcrumb');
+const searchInput = document.getElementById('search');
+const loading = document.getElementById('loading');
+const errorAlert = document.getElementById('error');
+const itemsPerPage = 10;
+
+let currentPath = '';
+
+function isFolder(key) {
+  return key.endsWith('/');
+}
+
+function createDownloadLink(key) {
+  const url = `https://${bucketName}.s3.amazonaws.com/${key}`;
+  const link = document.createElement('a');
+  link.href = url;
+
+  // Create the icon element
+  const icon = document.createElement('i');
+  icon.className = isFolder(key) ? 'fas fa-folder mr-2' : 'fas fa-file mr-2';
+
+  // Create the span element to hold the text
+  const textSpan = document.createElement('span');
+
+  if (isFolder(key)) {
+    textSpan.textContent = key.slice(0, -1).split('/').pop();
+  } else {
+    textSpan.textContent = key.split('/').pop();
+    link.setAttribute('download', '');
+  }
+
+  // Append the icon and the text span to the link
+  link.appendChild(icon);
+  link.appendChild(textSpan);
+
+  return link;
+}
+
+
+function navigateTo(path) {
+  currentPath = path;
+  listObjects(currentPath);
+}
+
+function updateBreadcrumb(path) {
+  const parts = path.split('/').filter((part) => part);
+  let currentPath = '';
+
+  breadcrumb.innerHTML = '<li class="breadcrumb-item"><a href="#">Home</a></li>';
+
+  parts.forEach((part, index) => {
+    currentPath += part + '/';
+    const listItem = document.createElement('li');
+    listItem.className = 'breadcrumb-item';
+
+    if (index === parts.length - 1) {
+      listItem.textContent = part;
+      listItem.classList.add('active');
+    } else {
+      const link = document.createElement('a');
+      link.href = '#';
+      link.textContent = part;
+      link.onclick = () => navigateTo(currentPath);
+      listItem.appendChild(link);
+    }
+
+    breadcrumb.appendChild(listItem);
+  });
+}
+
+function formatSize(size) {
+  if (isNaN(size)) {
+    return 'Unknown';
+  }
+
+  const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+  let index;
+
+  for (index = 0; size >= 1024 && index < units.length - 1; index++) {
+    size /= 1024;
+  }
+
+  return `${size.toFixed(2)} ${units[index]}`;
+}
+
+function listObjects(path) {
+  const prefix = path ? `prefix=${path}&` : '';
+  const url = `https://${bucketName}.s3.amazonaws.com/?list-type=2&${prefix}delimiter=%2F`;
+
+  loading.classList.remove('d-none');
+  errorAlert.classList.add('d-none');
+
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error fetching objects: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then((text) => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(text, 'text/xml');
+      const keys = xmlDoc.getElementsByTagName('Key');
+      const prefixes = xmlDoc.getElementsByTagName('Prefix');
+
+      objectList.innerHTML = '';
+
+      Array.from(prefixes).forEach((prefix) => {
+        const key = prefix.textContent;
+        const row = document.createElement('tr');
+        const nameCell = document.createElement('td');
+        const link = createDownloadLink(key);
+
+        link.onclick = (e) => {
+          e.preventDefault();
+          navigateTo(key);
+        };
+
+        nameCell.appendChild(link);
+        row.appendChild(nameCell);
+        row.insertCell(-1).textContent = ''; // Empty cells for last modified and size
+        row.insertCell(-1).textContent = '';
+        objectList.appendChild(row);
+      });
+
+      Array.from(keys).forEach((keyElement) => {
+        const key = keyElement.textContent;
+        if (key === 'index.html' || key === 's3.js' || key === 'dark-mode.css' || key === path) {
+          return;
+        }
+
+        const lastModified = new Date(keyElement.nextElementSibling.textContent);
+        const sizeElement = keyElement.parentNode.querySelector('Size');
+        const size = sizeElement ? parseInt(sizeElement.textContent, 10) : NaN;
+        const row = document.createElement('tr');
+        const nameCell = document.createElement('td');
+        const link = createDownloadLink(key);
+
+        nameCell.appendChild(link);
+        row.appendChild(nameCell);
+        row.insertCell(-1).textContent = lastModified.toLocaleString();
+        row.insertCell(-1).textContent = formatSize(size);
+        objectList.appendChild(row);
+      });
+
+      updateBreadcrumb(path);
+
+      loading.classList.add('d-none');
+    })
+    .catch((error) => {
+      console.error('Error fetching objects:', error);
+      loading.classList.add('d-none');
+      errorAlert.textContent = `Error fetching objects: ${error.message}`;
+      errorAlert.classList.remove('d-none');
+    });
+}
+
+searchInput.addEventListener('input', (e) => {
+  const filter = e.target.value.toLowerCase();
+  const rows = objectList.getElementsByTagName('tr');
+
+  for (let i = 0; i < rows.length; i++) {
+    const nameCell = rows[i].getElementsByTagName('td')[0];
+    const name = nameCell.textContent || nameCell.innerText;
+
+    if (name.toLowerCase().indexOf(filter) > -1) {
+      rows[i].style.display = '';
+    } else {
+      rows[i].style.display = 'none';
+    }
+  }
+});
+
+const darkModeSwitch = document.getElementById('darkModeSwitch');
+
+darkModeSwitch.addEventListener('change', (e) => {
+  const darkModeStyle = document.getElementById('dark-mode-style');
+  if (e.target.checked) {
+    darkModeStyle.disabled = false;
+    localStorage.setItem('darkMode', 'true');
+  } else {
+    darkModeStyle.disabled = true;
+    localStorage.setItem('darkMode', 'false');
+  }
+  
+});
+
+const darkModeStyle = document.getElementById('dark-mode-style');
+if (localStorage.getItem('darkMode') === 'true') {
+  darkModeSwitch.checked = true;
+  darkModeStyle.disabled = false;
+} else {
+  darkModeSwitch.checked = false;
+  darkModeStyle.disabled = true;
+}
+
+breadcrumb.onclick = (e) => {
+  e.preventDefault();
+  if (e.target.tagName === 'A') {
+    navigateTo('');
+  }
+};
+
+navigateTo('');
